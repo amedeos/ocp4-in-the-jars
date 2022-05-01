4ba7785fcf92c084bcf3fd0e7684ddd0fea14543
Amedeo Salvati <amedeo@linux.com>
2022-05-01 16:10:32 +0200

    make dynamic master and worker nodes

--- ansible-templates/root/baremetal-net.sh
+++ ansible-templates/root/baremetal-net.sh
@@ -8,8 +8,8 @@ nmcli connection down bridge-slave-eth1
 nmcli connection delete bridge-slave-eth1
 nmcli connection add ifname baremetal type bridge con-name baremetal
 nmcli con add type bridge-slave ifname eth1 master baremetal
-nmcli connection modify baremetal ipv4.addresses {{ bastion_bm_ip }}/24
-nmcli connection modify baremetal ipv4.gateway {{ bastion_bm_gw }}
+nmcli connection modify baremetal ipv4.addresses {{ bastion_nodes[0].baremetal_ip }}/{{ baremetal_net.prefix }}
+nmcli connection modify baremetal ipv4.gateway {{ baremetal_net.gateway }}
 nmcli connection modify baremetal ipv4.method manual
 nmcli connection modify baremetal ipv6.method ignore
 nmcli connection modify baremetal bridge.stp no
--- ansible-templates/root/provisioning-net.sh
+++ ansible-templates/root/provisioning-net.sh
@@ -8,8 +8,8 @@ nmcli connection down bridge-slave-eth0
 nmcli connection delete bridge-slave-eth0
 nmcli connection add ifname provisioning type bridge con-name provisioning
 nmcli con add type bridge-slave ifname eth0 master provisioning
-nmcli connection modify provisioning ipv4.addresses {{ bastion_prov_ip }}/24
-nmcli connection modify provisioning ipv4.dns {{ dns_ip_bastion }}
+nmcli connection modify provisioning ipv4.addresses {{ bastion_nodes[0].provisioning_ip }}/{{ provision_net.prefix }}
+nmcli connection modify provisioning ipv4.dns {{ utility_nodes[0].provisioning_ip }}
 nmcli connection modify provisioning ipv4.method manual
 nmcli connection modify provisioning ipv6.method ignore
 nmcli connection modify provisioning bridge.stp no
