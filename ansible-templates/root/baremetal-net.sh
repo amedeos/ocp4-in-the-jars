3e9e475047de40536d310bae2680f83087bae10f
Amedeo Salvati <amedeo@linux.com>
2022-09-10 12:38:42 +0200

    fix baremetal network creation on redfish

--- ansible-templates/root/baremetal-net.sh
+++ ansible-templates/root/baremetal-net.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/env bash
 #
-nmcli con down "System eth1"
-nmcli con delete "System eth1"
+{% if redfish.enable == True  %}
+INTERFACE="eth0"
+{% else %}
+INTERFACE="eth1"
+{% endif %}
+nmcli con down "System ${INTERFACE}"
+nmcli con delete "System ${INTERFACE}"
 nmcli connection down baremetal
 nmcli connection delete baremetal
-nmcli connection down bridge-slave-eth1
-nmcli connection delete bridge-slave-eth1
+nmcli connection down bridge-slave-${INTERFACE}
+nmcli connection delete bridge-slave-${INTERFACE}
 nmcli connection add ifname baremetal type bridge con-name baremetal
-nmcli con add type bridge-slave ifname eth1 master baremetal
-nmcli connection modify bridge-slave-eth1 802-3-ethernet.mtu {{ baremetal_net.mtu }}
+nmcli con add type bridge-slave ifname ${INTERFACE} master baremetal
+nmcli connection modify bridge-slave-${INTERFACE} 802-3-ethernet.mtu {{ baremetal_net.mtu }}
 nmcli connection modify baremetal ipv4.addresses {{ bastion_nodes[0].baremetal_ip }}/{{ baremetal_net.prefix }}
+
+{% if redfish.enable == True  %}
+nmcli connection modify baremetal ipv4.dns {{ utility_nodes[0].baremetal_ip }}
+{% endif %}
+
 nmcli connection modify baremetal ipv4.gateway {{ baremetal_net.gateway }}
 nmcli connection modify baremetal ipv4.method manual
 nmcli connection modify baremetal ipv6.method ignore
